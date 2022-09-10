2ae9909c59ccfa618449b05068f0d1a936896497
Filipe Garrote <filipe.garrote@gmail.com>
2022-09-10 12:11:28 -0300

    feat(nova saída financeira) - Implementada conexão com back-end, via axios, para remessa de nova saída financeira e sua listagem no histórico.

--- src/Components/MainPage/Outflow.js
+++ src/Components/MainPage/Outflow.js
@@ -2,15 +2,18 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-no-bind */
 /* eslint-disable react/jsx-filename-extension */
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import dayjs from 'dayjs';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { Form } from '../../Common/Form';
 import { Header } from '../../Common/Header';
+import UserContext from '../../Contexts/UserContext';
 
-export default function Outflow({ transacoesMockadas }) {
+export default function Outflow() {
   const [negativeEntry, setNegativeEntry] = useState({});
+  const { token } = useContext(UserContext);
   const navigate = useNavigate();
 
   function handleForm(e) {
@@ -20,18 +23,25 @@ export default function Outflow({ transacoesMockadas }) {
     });
   }
 
-  function submitForm(e) {
+  async function submitForm(e) {
     e.preventDefault();
     if (!negativeEntry.value || !negativeEntry.description) {
       alert('Todos os campos são de preenchimento obrigatório.\nPor favor, revise seus dados.');
       return;
     }
-    transacoesMockadas.push({
+    const transaction = {
       ...negativeEntry,
       date: dayjs(Date.now()).format('DD/MM'),
       type: 'saída',
-    });
-    alert('Sua movimentação foi registrada!! :)');
+    };
+
+    try {
+      await axios.post('http://localhost:5000/transactions', transaction, { headers: { Authorization: token } });
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    alert('Sua entrada foi registrada! :)');
     navigate('/account');
   }
 
